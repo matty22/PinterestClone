897ddba9c694f19c87fadf9e2039f205623566b8
Matt <matt.leonard.22@gmail.com>
2017-09-22 15:37:30 -0700

    copied in a lot of file s from a similar project

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// var mongoose = require('mongoose');
+
+// Production Database connection URL
+// var url = process.env.MongoURL;
+
+// // Localhost Database connection URL
+// // var url = 'mongodb://localhost:27017/nightlife';
+// mongoose.connect(url, {useMongoClient: true});
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+// });
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -11,19 +24,20 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// App routes here
 app.use('/', index);
-app.use('/users', users);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
