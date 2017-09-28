2c2e676c9d873e9df46377e454a2d5298d66b911
Matt <matt.leonard.22@gmail.com>
2017-09-27 20:00:27 -0700

    made login client js and express route functional

---
+++ public/javascripts/login.js
@@ -0,0 +1,27 @@
+// Function that creates new User object and sends it to Mongo
+function login() {
+  let userObj = {};
+
+  userObj.email = document.getElementById('email').value;
+  userObj.password = document.getElementById('password').value;
+  userObj.pollsVoted = [null];
+
+
+  // Setup data object to send to Express route
+  var json = JSON.stringify(userObj);
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'http://localhost:3000/users/login/data');
+  xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+  xhr.onload = function() {
+      if (xhr.status === 200) {
+        document.cookie = 'name=validLogin; path=/;'
+        // Only necessary if I'm going to try to prevent double votes
+        // sessionStorage.setItem('user', xhr.responseText)
+        window.location = '/books';
+      }
+      else {
+        document.getElementById('invalidLogin').style.display = 'inline-block';
+      }
+  }
+  xhr.send(json);
+}
--- routes/users.js
+++ routes/users.js
@@ -32,22 +32,22 @@ userRouter.get('/login', function(req, res, next) {
   res.render('../public/login');
 });
 
-// userRouter.route('/login/data')
-//           .post(function(req, res, next) {
-//             Users.find({'email': req.body.email})
-//                  .where('password').equals(req.body.password)
-//                  .exec(function(err, user) {
-//                    if (err) throw err;
-//                    if (user[0]) {
-//                      user.redirect = '/polls'
-//                      res.setHeader('Set-Cookie', ['type=login', 'language=javascript', 'path=/']);
-//                      res.send(user);
-//                    } else {
-//                      res.writeHead(401);
-//                      res.end('Bad Login');
-//                    }
-//                  })
-//           });
+userRouter.route('/login/data')
+          .post(function(req, res, next) {
+            Users.find({'email': req.body.email})
+                 .where('password').equals(req.body.password)
+                 .exec(function(err, user) {
+                   if (err) throw err;
+                   if (user[0]) {
+                     user.redirect = '/books'
+                     res.setHeader('Set-Cookie', ['type=login', 'language=javascript', 'path=/']);
+                     res.send(user);
+                   } else {
+                     res.writeHead(401);
+                     res.end('Bad Login');
+                   }
+                 })
+          });
 
 // This route just renders the edit page
 userRouter.get('/edit', function(req, res, next) {
